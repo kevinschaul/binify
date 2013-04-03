db9a12785dba0f342df9b433173405ff1910be21
Kevin Schaul <kevin.schaul@gmail.com>
2013-04-02 21:20:07 -0500

    Fixed hexagon shape math

--- binify/cli.py
+++ binify/cli.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
+import math
 import os
 import sys
 
 from osgeo import ogr
 from gdalconst import *
-
-def load_shapefile(filename):
-    """
-    Load a shapefile.
-    """
-    pass
 
 def parse_arguments():
     """
@@ -26,7 +21,6 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    load_shapefile(args.infile);
 
     driver = ogr.GetDriverByName('ESRI Shapefile')
     in_shapefile = driver.Open(args.infile, GA_ReadOnly)
@@ -45,14 +39,14 @@ def main():
     field_defn = ogr.FieldDefn('id', ogr.OFTInteger)
     out_layer.CreateField(field_defn)
 
-    create_grid(out_layer, 10, 10)
+    create_grid(out_layer, extent, 10, 10)
 
     in_shapefile.Destroy()
     out_shapefile.Destroy()
 
 def create_grid(layer, extent, num_x, num_y, *args, **kwargs):
     """
-    Creates `num_x * num_y` features to cover `extent` in `layer`.
+    Creates a grid of hexagon features in `layer`.
     """
     definition = layer.GetLayerDefn()
     width = extent[1] - extent[0]
@@ -66,28 +60,29 @@ def create_grid(layer, extent, num_x, num_y, *args, **kwargs):
     while x < extent[1]:
         x += scale_width
         hexagon = create_hexagon(x, y, scale_width)
-        feature = ogr.Feature(feature_defn)
+        feature = ogr.Feature(definition)
         feature.SetGeometry(hexagon)
         feature.SetField('id', 0)
-        out_layer.CreateFeature(feature)
+        layer.CreateFeature(feature)
         feature.Destroy()
 
 def create_hexagon(center_x, center_y, scale, *args, **kwargs):
     """
     Returns a hexagon geometry around the center point.
-    The resulting hexagon will have height `scale / 2` and width `scale`.
     """
-    scale_fourthed = scale / 4
-    scale_halved = scale / 2
+    h_val = math.sqrt(3) / 4
+    scale_quarter = scale / 4
+    scale_half = scale / 2
     ring = ogr.Geometry(ogr.wkbLinearRing)
-    # Draw hexagon, beginning with northwest vertice
-    ring.AddPoint(center_x - scale_fourthed, center_y + scale_fourthed)
-    ring.AddPoint(center_x + scale_fourthed, center_y + scale_fourthed)
-    ring.AddPoint(center_x + scale_halved, center_y)
-    ring.AddPoint(center_x + scale_fourthed, center_y - scale_fourthed)
-    ring.AddPoint(center_x - scale_fourthed, center_y - scale_fourthed)
-    ring.AddPoint(center_x - scale_halved, center_y)
-    ring.AddPoint(center_x - scale_fourthed, center_y + scale_fourthed)
+
+    # Draw hexagon clockwise, beginning with northwest vertice
+    ring.AddPoint(center_x - scale_quarter, center_y + h_val)
+    ring.AddPoint(center_x + scale_quarter, center_y + h_val)
+    ring.AddPoint(center_x + scale_half, center_y)
+    ring.AddPoint(center_x + scale_quarter, center_y - h_val)
+    ring.AddPoint(center_x - scale_quarter, center_y - h_val)
+    ring.AddPoint(center_x - scale_half, center_y)
+    ring.AddPoint(center_x - scale_quarter, center_y + h_val)
 
     hexagon = ogr.Geometry(type=ogr.wkbPolygon)
     hexagon.AddGeometry(ring)
