5a969eebd9e8d9eb5c53a3d41074578a7799ce18
Kevin Schaul <kevin.schaul@gmail.com>
2013-04-25 23:33:06 -0500

    Incremented version number
    To add --suppress-output to PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='binify',
-    version='0.1.3',
+    version='0.1.4',
     author='Kevin Schaul',
     author_email='kevin.schaul@gmail.com',
     url='http://www.kevinschaul.com',
